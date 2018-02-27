3e799f09158703c8f0cf5311ec84782cf1bf1f92
gamarin <gautier.marin@telecom-paristech.fr>
2018-02-27 16:24:52 +0100

    Fix proposal struct

--- x/gov/types/proposal.go
+++ x/gov/types/proposal.go
@@ -12,5 +12,4 @@ type Proposal struct {
 	VotingStartBlock     int64            //  Height of the block where MinDeposit was reached. -1 if MinDeposit is not reached
 	InitTotalVotingPower int64            //  Total voting power when proposal enters voting period (default 0)
 	InitProcedureNumber  int16            //  Procedure number of the active procedure when proposal enters voting period (default -1)
-	Votes                map[string]int64 //  Votes for each option (Yes, No, NoWithVeto, Abstain)
 }
